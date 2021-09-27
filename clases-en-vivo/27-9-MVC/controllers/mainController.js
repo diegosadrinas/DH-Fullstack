c355a8b4a14f44ee756a8395a31625acffedfef4
Diego Sadrinas <dsadrinas@gmail.com>
2021-09-27 12:55:19 -0300

    modifico el nombre de la carpeta

---
+++ clases-en-vivo/27-9-MVC/controllers/mainController.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+const controller = {
+    home: (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../views/home.html'))
+    },
+    about: (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../views/about.html'))
+    }
+};
+
+module.exports = controller;
